9c0f5718d893bb35fd51a8a3ce1504258aa6f307
Roman Matsuk <the.show.of.ice.man@gmail.com>
2021-01-13 19:14:44 +0200

    Updates

---
+++ precache-manifest.e52a66fe867497b196495adb246bfae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637436c5a2cfb443e0d51580005ac4a6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "21994c3b2fb30b661ba3",
+    "url": "./static/css/main.d0b1dc89.chunk.css"
+  },
+  {
+    "revision": "e7afe659128f626f5681",
+    "url": "./static/js/2.1727d404.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1727d404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21994c3b2fb30b661ba3",
+    "url": "./static/js/main.ec58e669.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
